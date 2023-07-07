f6f9bdeec0df8f740e43f0045f4d21f50a458433
anasselhaoussi <anous.elhaoussi@gmail.com>
2023-07-07 12:27:04 +0200

    file structure

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigation from '@/layout/Navigation'
+import Home from '@/layout/Home'
+
+export default function App() {
+  return (
+    <main>
+      <Navigation />
+      <Home />
+    </main>
+  )
+}
---
+++ src/layout/Navigation.tsx
@@ -0,0 +1,16 @@
+const Navigation = () => {
+  return (
+    <nav className="flex items-center gap-32">
+      <span>AnassDEV</span>
+      <ul className="flex gap-10">
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
